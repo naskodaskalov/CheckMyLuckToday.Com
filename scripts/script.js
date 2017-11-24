a1fe28da04486c4a385b71f8f487a6a424259d69
Nasko Daskalov <nasko.daskalov@abv.bg>
2017-11-24 12:27:11 +0200

    connect with firebase db

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,45 @@
-(function checkLuck () {
-  let arr = [ 'new friend',
-    'prosperity',
-    'money',
-    'travelling',
-    'luck',
-    'love',
-    'health',
-    'you will receive good news today',
-    'joy',
-    'the best friends are the pets.',
-    'there is no matter who say the speech, but who promt it.',
-    'surprice',
-    'it\'s time to relax',
-    'i had more problems with myself than with any other person',
-    'Things can be tough, Times can be rough. But if you work hard enough, You will achieve triumph!',
-    'Work hard and you will surely achieve success.',
-    'Today you will be calm and happy. All turn the way you want it to be.' ]
+$('document').ready(function () {
   let randomLuck = ''
-  let getLuck = setTimeout(() => {
-    if (window.localStorage.getItem('last-entrance')) {
-      let timeNow = new Date()
-      let lastEntranceTime = window.localStorage.getItem('last-entrance')
-      let lastLuck = window.localStorage.getItem('last-luck')
-      let nextDay = addDays(lastEntranceTime, 1)
 
-      if (timeNow < nextDay) {
-        $('.luck-of-the-day h2').css('display', 'none')
-        randomLuck = 'You can check your luck just one time a day. Come back after:'
-        $('#clockdiv').css('display', 'block')
-        $('.last-luck').css('display', 'block')
-        $('#last-luck').text(lastLuck).addClass('newLuck')
-        initializeClock('clockdiv', nextDay)
+  const baseUrl = 'https://checkmylucktodaydb.firebaseio.com/.json'
+  $.get(baseUrl)
+    .then((data) => {
+      let dbData = data['-KzhT0e5SZV7NK4tpGL5']
+      let dbLenght = dbData.length
+      if (window.localStorage.getItem('last-entrance')) {
+        let timeNow = new Date()
+        let lastEntranceTime = window.localStorage.getItem('last-entrance')
+        let lastLuck = window.localStorage.getItem('last-luck')
+        let nextDay = addDays(lastEntranceTime, 1)
+
+        if (timeNow < nextDay) {
+          $('.luck-of-the-day h2').css('display', 'none')
+          randomLuck = 'You can check your luck just one time a day. Come back after:'
+          $('#clockdiv').css('display', 'block')
+          $('.last-luck').css('display', 'block')
+          $('#last-luck').text(lastLuck).addClass('newLuck')
+          initializeClock('clockdiv', nextDay)
+        } else {
+          let randomNumber = Math.floor(Math.random() * dbLenght)
+          if (randomNumber < 1) {
+            randomNumber = 1
+          }
+          randomLuck = dbData[randomNumber]
+
+          console.log(randomLuck)
+          window.localStorage.setItem('last-luck', randomLuck)
+          window.localStorage.setItem('last-entrance', new Date())
+          if (randomLuck.length > 20) {
+            $('#current-luck').css('font-size', '40px')
+          }
+          $('#current-luck').addClass('newLuck')
+        }
       } else {
-        randomLuck = arr[Math.floor(Math.random() * arr.length)]
+        let randomNumber = Math.floor(Math.random() * dbLenght)
+        if (randomNumber < 1) {
+          randomNumber = 1
+        }
+        randomLuck = dbData[randomNumber]
         window.localStorage.setItem('last-luck', randomLuck)
         window.localStorage.setItem('last-entrance', new Date())
         if (randomLuck.length > 20) {
@@ -40,20 +47,14 @@
         }
         $('#current-luck').addClass('newLuck')
       }
-    } else {
-      randomLuck = arr[Math.floor(Math.random() * arr.length)]
-      window.localStorage.setItem('last-luck', randomLuck)
-      window.localStorage.setItem('last-entrance', new Date())
-      if (randomLuck.length > 20) {
-        $('#current-luck').css('font-size', '40px')
-      }
-      $('#current-luck').addClass('newLuck')
-    }
-    $('.intro').css('display', 'none')
-    $('#current-luck').text(randomLuck)
-    $('.luck-of-the-day').css('display', 'block')
-  }, 2000)
-})()
+      $('.intro').css('display', 'none')
+      $('#current-luck').text(randomLuck)
+      $('.luck-of-the-day').css('display', 'block')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 function addDays (date, days) {
   var result = new Date(date)
